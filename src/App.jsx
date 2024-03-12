c3517ada1eb6fd567ee43ee007d89969a3f732a5
MouhanadAhmed <houdacris88@gmail.com>
2024-03-12 21:29:02 +0200

    updated hash router for host

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import './App.css';
 import PageFlipSlider from './components/PageFlipSlider/PageFlipSlider.jsx';
 import Home from "./components/Home/Home.jsx"
 import Services from './components/Services/Services.jsx';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter,createHashRouter } from 'react-router-dom';
 import Layout from './components/Layout/Layout.jsx';
 import ServicePage from './components/ServicePage/ServicePage.jsx';
 import AboutUs from './components/AboutUs/AboutUs.jsx';
 import ContactUs from './components/ContactUs/ContactUs.jsx';
 import Portfolio from './components/Portfolio/Portfolio.jsx';
 function App() {
-  const routes = createBrowserRouter([
+  const routes = createHashRouter([
     {
       path:"",
       element: <Layout/> ,
