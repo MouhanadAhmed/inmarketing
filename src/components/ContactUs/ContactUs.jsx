1e828c5dea545e6383a837262ebcfe3caec55084
MouhanadAhmed <houdacris88@gmail.com>
2024-03-12 21:05:44 +0200

    updated contat form

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -77,7 +77,7 @@ export default function ContactUs() {
             <meta charSet                                                                   = "utf-8" />
             <title>IN - Contact Us</title>
         </Helmet>
-    <div className='bg-custom-green min-h-screen flex flex-wrap justify-center items-center'>
+    <div className='bg-custom-green min-h-screen flex flex-wrap justify-center items-center '>
       <div className="md:w-1/3">
         <img src={logo} alt="IN Marketing logo" />
       </div>
@@ -85,42 +85,42 @@ export default function ContactUs() {
       <p className='text-custom-cafe mb-3 font-medium'>Contact Us</p>
 
       <h2 className='text-custom-gold text-4xl md:text-5xl lg:text-5xl font-bold mb-3'>Get In Touch</h2>
-      <form onSubmit                                                                      = {formik.handleSubmit} onReset={formik.handleReset} {...initialvalues} className='flex flex-wrap  justify-center md:justify-start items-center '>
-      <div className                                                                            = "form-group mb-2 mx-auto md:mx-0   md:me-8 md:w-1/3">
+      <form onSubmit                                                                      = {formik.handleSubmit} onReset={formik.handleReset} {...initialvalues} className='flex flex-wrap  justify-evenly md:justify-evenly items-center mx-auto w-5/6'>
+      <div className                                                                            = "form-group mb-2  me-auto   w-1/2">
         {/* <label htmlFor                                                                              = "name" className={`fw-semibold text-uppercase `}>Name *</label> */}
-        <input type                                                                                 = "text" className={`form-control form-control-sm p-2 bg-transparent border text-custom-gold border-custom-gold  rounded-lg`}  id='name' name='name' placeholder='  Name' value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+        <input type                                                                                 = "text" className={`form-control form-control-sm p-2 w-5/6 me-6  bg-transparent border text-custom-gold border-custom-gold  rounded-lg`}  id='name' name='name' placeholder='  Name' value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
       </div>
      
-      <div className                                                                            = "form-group mb-2 mx-auto md:-mx-4  md:w-1/3">
+      <div className                                                                            = "form-group mb-2  w-1/2">
         {/* <label htmlFor                                                                              = "name" className={`fw-semibold text-uppercase `}>Name *</label> */}
-        <input type                                                                                 = "text" className={`form-control form-control-sm p-2 bg-transparent border text-custom-gold border-custom-gold  rounded-lg`}  id='company' name='company' placeholder='  Company' value={formik.values.company} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+        <input type                                                                                 = "text" className={`form-control form-control-sm w-full p-2 bg-transparent border text-custom-gold border-custom-gold  rounded-lg`}  id='company' name='company' placeholder='  Company' value={formik.values.company} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
       </div>
       {formik.errors.name && formik.touched.name?<p className                                   = 'p-2 mb-4 w-1/3 text-sm text-red-800 rounded-lg  dark:text-red-600  me-6  ' role="alert"> {formik.errors.name} </p>:"" }
       {formik.errors.company && formik.touched.company?<div className                                   = 'p-2 mb-4 text-sm text-red-800 rounded-lg  dark:text-red-600 w-1/3' role="alert">{formik.errors.company}</div>:"" }
 
-      <div className                                                                            = "form-group mb-2 md:w-5/6">
+      <div className                                                                            = "form-group mb-2 w-full">
         {/* <label htmlFor                                                                              = "name" className={`fw-semibold text-uppercase `}>Name *</label> */}
-        <input type                                                                                 = "text" className={`form-control form-control-sm p-2 md:w-5/6 bg-transparent border text-custom-gold border-custom-gold  rounded-lg`}  id='website' name='website' placeholder='  website or social media link' value={formik.values.website} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+        <input type                                                                                 = "text" className={`form-control w-full form-control-sm p-2  bg-transparent border text-custom-gold border-custom-gold  rounded-lg`}  id='website' name='website' placeholder='  website or social media link' value={formik.values.website} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
       </div>
      
-      <div className                                                                            = "form-group mb-2 mx-auto md:mx-0   md:me-8 md:w-1/3">
+      <div className                                                                            = "form-group mb-2  w-1/2">
         {/* <label htmlFor                                                                              = "name" className={`fw-semibold text-uppercase `}>Name *</label> */}
-        <input type                                                                                 = "text" className={`form-control form-control-sm p-2 bg-transparent border text-custom-gold border-custom-gold  rounded-lg`}  id='phone' name='phone' placeholder='  Phone' value={formik.values.phone} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+        <input type                                                                                 = "text" className={`form-control form-control-sm w-5/6  me-6 p-2 bg-transparent border text-custom-gold border-custom-gold  rounded-lg`}  id='phone' name='phone' placeholder='  Phone' value={formik.values.phone} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
       </div>
       
-      <div className                                                                            = "form-group mb-2 mx-auto md:-mx-4  md:w-1/3">
+      <div className                                                                            = "form-group mb-2    w-1/2">
         {/* <label htmlFor                                                                              = "name" className={`fw-semibold text-uppercase `}>Name *</label> */}
-        <input type                                                                                 = "text" className={`form-control form-control-sm p-2 bg-transparent border text-custom-gold border-custom-gold  rounded-lg`}  id='email' name='email' placeholder='  Email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+        <input type                                                                                 = "text" className={`form-control form-control-sm w-full p-2 bg-transparent border text-custom-gold border-custom-gold  rounded-lg`}  id='email' name='email' placeholder='  Email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
       </div>
 
-      <div className                                                                            = "form-group mb-2 md:w-5/6">
+      <div className                                                                            = "form-group mb-2 w-full">
         {/* <label htmlFor                                                                              = "name" className={`fw-semibold text-uppercase `}>Name *</label> */}
-        <input type                                                                                 = "text" className={`form-control form-control-sm p-2 md:w-5/6 bg-transparent border text-custom-gold border-custom-gold  rounded-lg`}  id='subject' name='subject' placeholder='  Service type' value={formik.values.subject} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+        <input type                                                                                 = "text" className={`form-control form-control-sm p-2 w-full bg-transparent border text-custom-gold border-custom-gold  rounded-lg`}  id='subject' name='subject' placeholder='  Service type' value={formik.values.subject} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
       </div>
 
-      <div className                                                                              = "form-group mb-3  md:w-5/6">
+      <div className                                                                              = "form-group mb-3  md:w-full">
         {/* <label htmlFor                                                                              = "notes" className={`fw-semibold text-uppercase ${styles.labelText}`}>{t("NoteAboutTheVisitor")}</label> */}
-        <textarea rows                                                                              = "4" cols="48" className={`form-control form-control-sm py-2 bg-transparent border text-custom-gold border-custom-gold  rounded-lg resize-none`}  id='message'name='message' value={formik.values.message} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+        <textarea rows                                                                              = "4" cols="40" className={`form-control form-control-sm w-full py-2 bg-transparent border text-custom-gold border-custom-gold  rounded-lg resize-none`}  id='message'name='message' value={formik.values.message} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
       </div>
       
 
